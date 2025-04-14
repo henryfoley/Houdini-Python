6aa84577867e0ee5e980a082f1fd3826c475c1ab
Henry Foley <henry.foley@mymail.champlain.edu>
2025-04-14 11:33:16 -0400

    Refactored functions
    Removed some hard coded values

--- Files/DefaultPyProject/scripts/python/CLIP_model.py
+++ Files/DefaultPyProject/scripts/python/CLIP_model.py
@@ -7,23 +7,12 @@ from transformers import CLIPProcessor, CLIPModel
 model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
 processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
-def make_flipbook(start, end, filename = ''):
+def make_flipbook(start, end, camera_node, filename = ''):
+    # Init Scene
     scene : hou.SceneViewer = toolutils.sceneViewer()
     view : hou.GeometryViewport = scene.curViewport()
 
-    camera_path = "/obj/cam1"
-    camera_node : hou.GeometryViewportCamera = hou.node(camera_path)
-    root = hou.node('/')
-    camera_nodes = root.recursiveGlob('*', hou.nodeTypeFilter.ObjCamera)
-    camera_node = camera_nodes[0]
-    if camera_node:
-        print("cam1 found")
-    else:
-        for camera in camera_nodes:
-            camera : hou.GeometryViewportCamera
-            print(f"Camera: {camera.path()}")
-
-    original_camera = view.camera()
+    # Set Camera
     view.setCamera(camera_node)
 
     # Create a temporary file if filename is not provided
@@ -32,22 +21,19 @@ def make_flipbook(start, end, filename = ''):
         filename = temp_file.name
         temp_file.close()
 
-    #Flipbook settings
+    # Flipbook settings
     flip_options :hou.FlipbookSettings = scene.flipbookSettings().stash()
     flip_options.frameRange((start,end))
     flip_options.output(filename)
 
-    #use camera resoltion if available
+    # Use camera resoltion if available
     if scene.curViewport().camera():
         flip_options.useResolution(True)
         flip_options.resolution((scene.curViewport().camera().evalParm('resx'), scene.curViewport().camera().evalParm('resy') ))
         flip_options.cropOutMaskOverlay(True)
   
+    # Create Flipbook
     scene.flipbook(view, flip_options)
-
-    # Restore the original camera
-    if camera_path and original_camera:
-        view.setCamera(original_camera)
     
     return filename
 
@@ -57,10 +43,8 @@ geo.addAttrib(hou.attribType.Point, "class", "")
 geo.addAttrib(hou.attribType.Point, "prob", 0.0)
 
 # %% Zero-shot classification
-def run_model(image_filename):
-    #image_filename = "images/hotdog.jpg"
+def run_model(image_filename, classes):
     image = Image.open(image_filename)
-    classes = ['pig', 'man', 'squid', 'hotdog']
     inputs = processor(text=classes, images=image, return_tensors="pt", padding=True)
 
     outputs = model(**inputs)
