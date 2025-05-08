0ed06eb96a75860f6f3edd97b0018f2ae404f917
Henry Foley <henry.foley@mymail.champlain.edu>
2025-05-08 14:29:14 -0400

    Multi-Cam Working
    Created system for running the model on multiple angles of the model and keeping the highest score

--- Files/DefaultPyProject/scripts/python/CLIP_model.py
+++ Files/DefaultPyProject/scripts/python/CLIP_model.py
@@ -34,12 +34,6 @@ def make_flipbook(start, end, camera_node, filename = ''):
     
     return filename
 
-node = hou.pwd()
-geo : hou.Geometry = node.geometry()
-geo.addAttrib(hou.attribType.Point, "class", "")
-geo.addAttrib(hou.attribType.Point, "prob", 0.0)
-geo.addAttrib(hou.attribType.Point, "camera", "")
-
 # %% Zero-shot classification
 def run_model(model, processor, image_filename, classes):
     image = Image.open(image_filename)
@@ -49,10 +43,13 @@ def run_model(model, processor, image_filename, classes):
     logits_per_image = outputs.logits_per_image  # this is the image-text similarity score
     probs = logits_per_image.softmax(dim=1)  # we can take the softmax to get the label probabilities
     probs = probs[0].detach().numpy()
-
-    """for id in range(len(probs)):
-        point : hou.Point = geo.createPoint()
-        point.setAttribValue("class", classes[id])
-        point.setAttribValue("prob", float(probs[id]))"""
     
     return probs
+
+def reset_viewport(camera_nodes):
+    # Init Scene
+    scene : hou.SceneViewer = toolutils.sceneViewer()
+    view : hou.GeometryViewport = scene.curViewport()
+
+    # Set Camera
+    view.setCamera(camera_nodes[0])
